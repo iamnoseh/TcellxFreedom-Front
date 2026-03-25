6335ac696ee325ce80791dc5c548e889ea6af657
Noseh Taghaymurodzoda <noseh.tagaymurodzoda@gmail.com>
2026-03-25 17:38:17 +0500

    feat(api-client): update baseURL to use environment variable for API URL

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import { useAuthStore } from '@/lib/store/auth.store'
 
-export const apiClient = axios.create({ baseURL: 'http://localhost:5035' })
+export const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5035',
+})
 
 apiClient.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token
